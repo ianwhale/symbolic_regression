22c9209906b6a09895a79d0a829eab0cc44ed0e5
Ian <ianwhale@github.com>
2018-11-18 10:52:05 -0500

    Add option to run only tests in a particular directory so run_test.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-if [ ! -f ./catch_main.o ]; then
-    g++ --std=c++11 catch_main.cpp -o catch_main.o
-fi
-
-for dir in $(find . -maxdepth 1 -mindepth 1 -type d);
-do
-    for test in $dir/*.cpp;
+function compile_and_run_tests_in_dir {
+    for test in $1/*.cpp;
     do
         g++ --std=c++11 catch_main.o $test -fopenmp
         ./a.out
     done
-done
+}
+
+if [ ! -f ./catch_main.o ]; then
+    g++ --std=c++11 catch_main.cpp -o catch_main.o
+fi
+
+if [ $# -gt 0 ]; then
+    # Do only tests in directories provided.
+    while [ "$1" != "" ]; do
+        compile_and_run_tests_in_dir $1
+        shift
+    done
+else
+    # Do all tests.
+    for dir in $(find . -maxdepth 1 -mindepth 1 -type d);
+    do
+        compile_and_run_tests_in_dir $dir
+    done
+fi
