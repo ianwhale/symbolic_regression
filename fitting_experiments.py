80e2b50b227c965a65e31ab7bfe8cdb533ff8267
Ian <ianwhale@github.com>
2018-12-03 09:13:20 -0500

    Update fitting experiment script.

--- fitting_experiments.py
+++ fitting_experiments.py
@@ -1,12 +1,11 @@
-#
-# Generate experiment scripts to fit sin, exp, and ln
-#
 import os
 import sys
+from itertools import product
 
 #
-# Generate SLURM scripts for the between node experiments.
+# Generate experiment scripts to fit sin, exp, and ln and tune hyperparams.
 #
+
 base_str = """#!/bin/bash
 #SBATCH --mail-user={email}
 #SBATCH --mail-type=FAIL
@@ -22,7 +21,7 @@ base_str = """#!/bin/bash
 cd ${{SLURM_SUBMIT_DIR}}
 
 export OMP_NUM_THREADS={tasks},{nested}
-srun -n {nodes} run.out -m 0.01 -c 0.75 -s ${{SLURM_ARRAY_TASK_ID}} -f {func} -p 250 -g 10000 -o ./{output}/
+srun -n {nodes} run.out -m {mutation_rate} -c {crossover_rate} -s ${{SLURM_ARRAY_TASK_ID}} -f {func} -p 250 -g 10000 -o ./{output}/
 
 scontrol show job ${{SLURM_JOB_ID}}
 """
@@ -33,21 +32,27 @@ except IndexError:
     print("Must provide email, buy-in group name, and output directory.")
 
 functions = [1, 2, 3]
+mutation_rate = [0.001, 0.005, 0.01, 0.05, 0.10, 0.15, 0.2]
+crossover_rate = [0.5, 0.6, 0.7, 0.8]
 map_to_name = {1: "log", 2: "exp", 3: "sin"}
 
-for func in functions:
-    name = map_to_name[func]
-    this_output = os.path.join(output, name)
-    os.makedirs(this_output, exist_ok=True)
 
-    with open(name + ".sb", "w") as fptr:
-        fptr.write(base_str.format(
-            email=email,
-            name=name,
-            buyin=buyin,
-            output=this_output,
-            nodes=2,
-            tasks=14,
-            nested=2,
-            func=func
-        ))
+for func in functions:
+    for m, cr in product(mutation_rate, crossover_rate):
+        name = map_to_name[func] + "m{}cr{}".format(str(m).replace(".", "_"), str(cr).replace(".", "_"))
+        this_output = os.path.join(output, name)
+        os.makedirs(this_output, exist_ok=True)
+
+        with open(name + ".sb", "w") as fptr:
+            fptr.write(base_str.format(
+                email=email,
+                name=name,
+                buyin=buyin,
+                output=this_output,
+                nodes=2,
+                tasks=14,
+                nested=2,
+                func=func,
+                mutation_rate=m,
+                crossover_rate=cr
+            ))
